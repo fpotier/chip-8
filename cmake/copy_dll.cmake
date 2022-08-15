10069fa322f75efab49ce0543f1a9a8dd443b200
fpotier <frederic.potier@protonmail.com>
2022-08-15 21:11:24 +0200

    Build: `copy_dll()` now takes a list as input

--- cmake/copy_dll.cmake
+++ cmake/copy_dll.cmake
@@ -1,9 +1,11 @@
-function(copy_dll exe dll)
-    if (WIN32 AND ${BUILD_SHARED_LIBS})
-        add_custom_command(TARGET ${exe} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            $<TARGET_FILE:${dll}>
-            $<TARGET_FILE_DIR:${exe}>
-        )
-    endif()
+function(copy_dll exe dll_list)
+    foreach (dll ${dll_list})
+        if (WIN32 AND ${BUILD_SHARED_LIBS})
+            add_custom_command(TARGET ${exe} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                $<TARGET_FILE:${dll}>
+                $<TARGET_FILE_DIR:${exe}>
+            )
+        endif()
+    endforeach()
 endfunction()
