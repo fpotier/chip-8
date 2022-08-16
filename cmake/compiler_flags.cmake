9f31b0972af27dea7c7d8e1b9b713638c613fadb
fpotier <frederic.potier@protonmail.com>
2022-08-16 11:17:51 +0200

    Build: refactor cmake
    - Simplify using generator expressions
    - Always fetch SDL2 instead of using find_package
    - Do not enable warnings on external projects

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,14 +1,9 @@
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-if (MSVC)
-    add_compile_options(/W3)
-else()
-    add_compile_options(-Wall -Wextra -Wpedantic)
-endif()
-
-if("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
-    add_compile_options(-fdiagnostics-color=always)
-elseif("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang")
-    add_compile_options(-fcolor-diagnostics)
-endif()
+function(enable_warnings target)
+    target_compile_options(${target} PRIVATE
+        $<$<CXX_COMPILER_ID:GNU>: -Wall -Wextra -Wpedantic -fdiagnostics-color=always>
+        $<$<CXX_COMPILER_ID:Clang>: -Wall -Wextra -Wpedantic -fcolor-diagnostics>
+        $<$<CXX_COMPILER_ID:MSVC>: /W3>)
+endfunction()
