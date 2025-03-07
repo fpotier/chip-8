4bbe16bd669b4ec62e5584787c3d67eef8bf370d
fpotier <frederic.potier@protonmail.com>
2025-03-07 09:18:56 +0100

    Remove cpp code

--- cmake/compiler_flags.cmake
+++
@@ -1,9 +0,0 @@
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
-function(enable_warnings target)
-    target_compile_options(${target} PRIVATE
-        $<$<CXX_COMPILER_ID:GNU>: -Wall -Wextra -Wpedantic -fdiagnostics-color=always>
-        $<$<CXX_COMPILER_ID:Clang>: -Wall -Wextra -Wpedantic -fcolor-diagnostics>
-        $<$<CXX_COMPILER_ID:MSVC>: /W3>)
-endfunction()
